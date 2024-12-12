3622777f026fb261a4b57cb8a68264c6d3c8c3e0
sgKim <sgkim925@gmail.com>
2024-12-12 09:01:54 +0800

    Add App.java

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import utils.Test;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Leetcode leet = new Leetcode();
+        int[] nums = {2,3,6,8,4};
+        System.out.println(leet.findPaths(10,10,0,5,5));
+    }
+}
